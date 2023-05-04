a35a22fb1be25f9b748a220634115c24a41cbeb3
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-04 21:54:47 +0700

    Config-App

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+const PORT = 8000;
+
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+
+app.use(bodyParser.json({
+    limit: "50mb",
+}));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}/`);
+});
+
+
