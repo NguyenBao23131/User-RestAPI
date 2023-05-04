f4f13b3d985f5ea20a0dd2a279b87881c5a5c321
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-04 22:26:13 +0700

    update-MongoDB-Connect

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,19 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import * as dotenv from "dotenv";
+
+import mongoose from "mongoose";
+
+// ConfigApp
+dotenv.config();
 
 const app = express();
 const PORT = 8000;
 
+mongoose.set('strictQuery', false);
 
+// use method
 app.use(cors({
     credentials: true,
 }));
@@ -24,10 +32,26 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// Connect Server and DB
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}/`);
 });
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            autoIndex: true,
+            autoCreate: true,
+        });
+        console.log("Connected to MongoDB successfully");
+    } catch (error) {
+        console.log(`Error connecting to MongoDB: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+connectDB();
 
 
