d947639bf565770ee69f4bff3b9e6c6ab85047f4
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-05-05 23:03:33 +0700

    update-Register

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,13 +2,15 @@ import crypto from "crypto";
 
 const SECRET = "REST_API_TS"
 
-export const random = () => {
-    crypto.randomBytes(128).toString('base64');
+export const random = (): string => {
+    return crypto.randomBytes(128).toString('base64');
 };
 
-export const authentication = (salt: string, password: string) => {
+export const authentication = (salt: string, password: string): string => {
     return crypto.createHmac(
         'sha256',
         [salt, password].join("/")
     ).update(SECRET).digest('hex');
 };
+
+
